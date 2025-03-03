e2517df045169bd8460b25465c40958a6b95b483
hanch2932 <hanch2932@gmail.com>
2025-03-03 20:21:10 +0900

    CMake 설정 개선: third-party.cmake 파일로 서드파티 패키지 관리 분리

---
+++ cmake/third-party.cmake
@@ -0,0 +1,19 @@
+find_package(Vulkan REQUIRED)
+find_package(SDL3)
+if(NOT SDL3_FOUND)
+  message("SDL3 패키지를 찾을 수 없습니다. 직접 빌드합니다.")
+  include(sdl)
+endif()
+
+include(imgui)
+
+add_library(
+  third-party
+  INTERFACE
+)
+
+target_link_libraries(
+  third-party
+  INTERFACE
+  imgui # sdl3 포함
+)
