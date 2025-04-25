76761ba9f42c07bdd77aaf0ac08ce40f5a77757f
hanch2932 <hanch2932@gmail.com>
2025-04-26 03:08:59 +0900

    setup.sh: 초기 설정 메시지를 'MSYS2 초기 설정 중...'에서 'pacman 초기 설정 중...'으로 변경

--- scripts/windows/bash/setup.sh
+++ scripts/windows/bash/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # /etc/pacman.conf 설정 값 수정
-echo "MSYS2 초기 설정 중..."
+echo "pacman 초기 설정 중..."
 sed -i 's/^#\?\s*ParallelDownloads\s*=.*/ParallelDownloads = 10/' /etc/pacman.conf
 sed -i 's/^\s*CheckSpace/#&/' /etc/pacman.conf
 sed -i 's/^#\?\s*VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf
